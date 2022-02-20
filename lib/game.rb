97f651ae863cd1ab821c72d2954e3ef6245583aa
Ricardo <ricardomg92@hotmail.com>
2022-02-20 21:56:35 +0100

    load dictionary file and choose a random word between 5 and 12 letters

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,25 @@
 
 # Contains the game logic
 class Game
+  def choose_word
+    word = load_dictionary[rand(load_dictionary.length)]
+    if word.length.between?(5, 12)
+      word
+    else
+      choose_word
+    end
+  end
+
+  def load_dictionary
+    if File.exist?('english_words.txt')
+      File.read('english_words.txt').split
+    else
+      puts 'Dictionary file was not found!'
+      exit
+    end
+  end
+
   def play
-    puts 'hello'
+    puts choose_word
   end
 end
