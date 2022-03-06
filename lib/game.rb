908bf06b04ec44626e915f6b436c4e6caed89801
Ricardo <ricardomg92@hotmail.com>
2022-03-06 21:27:54 +0100

    Adds load game functionality

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'display'
+require 'yaml'
 
 # Contains the game logic
 class Game
@@ -16,6 +17,7 @@ class Game
 
   def play
     display_rules
+    load_game if gets.chomp.to_i.eql?(1)
     display_game_start
     play_round until @game_end
     display_result
@@ -29,7 +31,7 @@ class Game
   end
 
   def player_input
-    puts 'Input 1 to save your game and exit, input a letter or word to guess'
+    puts 'Input 1 to save your game and exit, input a letter or word to make a guess'
     input = gets.chomp.downcase
     if input.to_i.eql?(1)
       save_game
@@ -87,15 +89,23 @@ class Game
   end
 
   def save_game
-    # save game method
-    # check if save folder exists
     Dir.mkdir('./saved_games') unless Dir.exist?('./saved_games')
     filename = "./saved_games/#{choose_word}_#{choose_word}.yaml"
-    File.open(filename, 'w')
-    #   create save folde if it doesn't exist
-    # create and yaml file
-    # serialize the game object to it
+    File.open(filename, 'w') { |file| YAML.dump([] << self, file) }
+    puts 'Game Saved. Goodbye!'
     exit
+  end
+
+  def load_game
+    puts 'Wich game do you want to load?'
+    save_list = []
+    Dir.glob('**', base: './saved_games').each { |file| save_list << file.to_s }
+    save_list.each_with_index { |file, index| puts "#{index}: #{file}" }
+    save_game = YAML.load_file("./saved_games/#{save_list[gets.to_i]}", permitted_classes: [Game])
+    @word = save_game[0].instance_variable_get('@word')
+    @right_guesses = save_game[0].instance_variable_get('@right_guesses')
+    @wrong_guesses = save_game[0].instance_variable_get('@wrong_guesses')
+    @remaining_guesses = save_game[0].instance_variable_get('@remaining_guesses')
   end
 
   def load_dictionary
